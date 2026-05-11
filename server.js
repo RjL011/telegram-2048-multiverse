a31612d01127e4dc2d95f7508baac6e79e8a57ee
RjL011 <rasovtm@gmail.com>
2026-05-11 21:05:38 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const root = __dirname;
+const mime = {
+  ".html": "text/html; charset=utf-8",
+  ".js": "application/javascript",
+  ".css": "text/css",
+  ".json": "application/json",
+  ".ico": "image/x-icon",
+  ".png": "image/png",
+  ".svg": "image/svg+xml",
+};
+const PORT = Number(process.env.PORT) || 3000;
+http
+  .createServer((req, res) => {
+    let u = (req.url || "/").split("?")[0];
+    if (u === "/") u = "/index.html";
+    const file = path.normalize(path.join(root, u));
+    if (!file.startsWith(root)) {
+      res.writeHead(403);
+      return res.end();
+    }
+    fs.stat(file, (err, st) => {
+      if (err || !st.isFile()) {
+        res.writeHead(404);
+        return res.end("Not found");
+      }
+      const ext = path.extname(file).toLowerCase();
+      res.writeHead(200, { "Content-Type": mime[ext] || "application/octet-stream" });
+      fs.createReadStream(file).pipe(res);
+    });
+  })
+  .listen(PORT, "0.0.0.0", () => {
+    console.log("http://localhost:" + PORT + "/");
+  });
